798374e162b41b029d3ff22cbde0a2cee084d830
soomin Kang <tnals3256@naver.com>
2023-12-22 12:24:23 +0900

    12월 22일 오후

---
+++ src/main/java/common/JDBCUtil.java
@@ -0,0 +1,40 @@
+package common;
+
+import java.sql.*;
+
+public class JDBCUtil {
+
+	 // DataBase 연결 ( Connection ) 객체 : Oracle
+	
+		// 기본 생성자
+		public JDBCUtil () {
+			System.out.println(" JDBC 호출 성공 ");
+		}
+		
+		// static 메소드 : 객체 생성 없이 클래스 이름으로 호출
+		public static Connection getConnection () {
+			Connection conn = null;
+			
+			//오라클 DB : XE , C##HR, 1234, <== 연결하는 구문 생성
+			String driver = "oracle.jdbc.driver.OracleDriver"; 
+		    String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		     
+		     
+		     try{
+		    	
+		    	 Class.forName(driver);  
+		         conn = DriverManager.getConnection(url,"C##HR", "1234");
+		    	// System.out.println("DB 연결이 성공 했습니다.");			 
+		    	 
+		     }catch (Exception e) {
+		    	 // catch 블락 : try 블락의 오류가 있을때 만 작동됨
+		    	 
+		    	 System.out.println("DB 연결이 실패 했습니다. ");
+		    	
+		         // DB연결이 실패시 오류난 정보를 콘솔에 출력
+		         e.printStackTrace();    	 
+		     }
+			return conn;
+		}
+		
+}
---
+++ src/main/java/test/DB_Connection_Test.java
@@ -0,0 +1,19 @@
+package test;
+
+import common.JDBCUtil;
+import java.sql.*;
+
+public class DB_Connection_Test {
+
+	public static void main(String[] args) {
+              
+		      // DB 연결 테스트
+              JDBCUtil.getConnection();
+              
+              // Oracle 연결된 conn 객체를 받는다.
+              Connection conn = JDBCUtil.getConnection();
+	
+
+	}
+
+}
